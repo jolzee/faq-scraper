662912df3413fe29311d93638aee1c87ad6cbbf6
Peter Joles <jolzee@gmail.com>
2020-04-12 12:56:12 -0700

    slugify parent and child keys

--- client/src/components/Scraper.vue
+++ client/src/components/Scraper.vue
@@ -210,6 +210,25 @@ import copy from "copy-to-clipboard";
 const clonedeep = require("lodash/cloneDeep");
 import cronstrue from "cronstrue";
 var isEqual = require("lodash/isEqual");
+
+function slugify(string) {
+  const a =
+    "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;";
+  const b =
+    "aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------";
+  const p = new RegExp(a.split("").join("|"), "g");
+
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w-]+/g, "") // Remove all non-word characters
+    .replace(/--+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, ""); // Trim - from end of text
+}
 // import { mapGetters } from "vuex";
 export default {
   name: "Scraper",
@@ -254,6 +273,8 @@ export default {
   watch: {
     configCopy: {
       handler(newVal) {
+        newVal.parentKey = slugify(newVal.parentKey);
+        newVal.childKey = slugify(newVal.childKey);
         let isEqualObjs = isEqual(newVal, this.config);
         this.isDirty = !isEqualObjs;
       },
